a046f34becb8063513e348dd263364ebe7a4c07c
Alex Solomaha <cyanofresh@gmail.com>
2015-06-01 23:27:21 +0300

    Page migrations

--- common/config/main.php
+++ common/config/main.php
@@ -23,17 +23,6 @@ return [
             'class' => 'yz\shoppingcart\ShoppingCart',
             'cartId' => 'yii2shop',
         ],
-        'view' => [
-            'class' => '\rmrevin\yii\minify\View',
-            'enableMinify' => !YII_DEBUG,
-            'web_path' => '@web', // path alias to web base
-            'base_path' => '@webroot', // path alias to web base
-            'minify_path' => '@webroot/assets', // path alias to save minify result
-            'js_position' => [ \yii\web\View::POS_END ], // positions of js files to be minified
-            'force_charset' => 'UTF-8', // charset forcibly assign, otherwise will use all of the files found charset
-            'expand_imports' => true, // whether to change @import on content
-            'compress_output' => true, // compress result html page
-        ],
         'formatter' => [
             'currencyCode' => 'UAH',
         ],
---
+++ console/migrations/m150601_202439_create_page_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150601_202439_create_page_table extends Migration
+{
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%page}}', [
+            'id' => Schema::TYPE_PK,
+            'menu' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'slug' => Schema::TYPE_STRING . ' NOT NULL',
+            'name' => Schema::TYPE_STRING . ' NOT NULL',
+            'body' => Schema::TYPE_TEXT,
+            'meta_description' => Schema::TYPE_STRING . ' NOT NULL',
+            'meta_keywords' => Schema::TYPE_STRING . ' NOT NULL',
+        ], $tableOptions);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%status}}');
+    }
+}

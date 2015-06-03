8f9b01a577b5b9576c520ea0d783477e1c982e35
Alex Solomaha <cyanofresh@gmail.com>
2015-06-03 22:56:11 +0300

    Migrations fixes

---
+++ console/migrations/m150603_195415_create_subscribers_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150603_195415_create_subscribers_table extends Migration
+{
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%subscriber}}', [
+            'id' => Schema::TYPE_PK,
+            'date' => Schema::TYPE_DATETIME,
+            'email' => Schema::TYPE_STRING . ' NOT NULL',
+            'name' => Schema::TYPE_STRING . ' NOT NULL',
+        ], $tableOptions);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%subscriber}}');
+    }
+}

bf6a4cc0aadfcaf2c6f810779982afc3e2fb831c
Alex Solomaha <cyanofresh@gmail.com>
2015-06-06 18:14:23 +0300

    Navbar hides

--- frontend/web/js/site.js
+++ frontend/web/js/site.js
@@ -1,15 +1,34 @@
-// Main site javascript
-var form = $('#subscribe-form');
-form.submit(function() {
-    $.post(form.attr('action'), form.serialize(), function(data) {
-        console.log(data);
-        if (data.type == 'success') {
-            form.html(data.body);
-            $('#result').html(null);
+// Hide navbar-top when scrolling
+$(function () {
+    var scroll = 0;
+    $(window).scroll(function () {
+        var ts = $(this).scrollTop();
+        if (ts > scroll) {
+            $('.navbar').addClass('stuck');
         } else {
-            $('#result').html(data.body);
+            $('.navbar').removeClass('stuck');
         }
-    }, 'json');
+        if (ts < 600) {
+            $('.navbar').removeClass('stuck');
+        }
+        scroll = ts;
+    });
+});
 
-    return false;
+// Subscribe form handling
+$(function () {
+    var form = $('#subscribe-form');
+    form.submit(function () {
+        $.post(form.attr('action'), form.serialize(), function (data) {
+            console.log(data);
+            if (data.type == 'success') {
+                form.html(data.body);
+                $('#result').html(null);
+            } else {
+                $('#result').html(data.body);
+            }
+        }, 'json');
+
+        return false;
+    });
 });
